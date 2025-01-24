cb36876224dbecd4f20678774815019ed40e830d
darklight9911 <muniaedu911@gmail.com>
2025-01-25 00:43:34 +0600

    prob 16 solved finally

--- lab-2/string/lab-16.c
+++ lab-2/string/lab-16.c
@@ -2,36 +2,25 @@
 int main(){
     char string_1[300];
     fgets(string_1, 300, stdin);
-    int counter = 0,length = 0,count = 1,left;
-    int arr[100];
-    while(string_1[length] != '\n'){length++;}
-    // printf("length:%d\n",length);
-    for(int i = 0;i<length; i++){
-        if(string_1[i]== ' '){
-            if(counter == 0){
-                arr[counter] = i;}
-            else{
-                arr[counter] = i - arr[counter-1]-count;count++;left = i;printf("%d\n",left);}
-            counter++;
-        }
-        else if(i==(length-1)){
-            arr[counter] = length-1 -left;printf("count:%d, last ele of arr:%d\n",count,arr[counter]);
-            // printf("%d",counter);
-        }
-        else{continue;}
-    }
-    int long_word=0;
-    int index = 0;
-    for(int i = 0;i<=counter; i++){
-        printf("%d ",arr[i]);
-        if(long_word <= arr[i]){
-            long_word = arr[i];
-            index = arr[i-1];
+    int counter = 0,length = 0,left=0;
+    // int arr[100];
+    while(string_1[length] != '\n'&& string_1[length] != '\0'){length++;}
+    if (string_1[length] == '\n') {string_1[length] == '\0';}
+    int max_len = 0, max_ind = 0;
+    for(int  i = 0;i <= length;i++){
+        if(string_1[i] == ' '||i== length|| string_1[i] == '\0' ){
+            int w_len = i - left;
+            // arr[counter++] = w_len;
+            if(w_len>=max_len){
+                max_len = w_len;
+                max_ind = left;
+            }
+            left = i + 1;
         }
     }
-    for(int i = (index+2); i<= (long_word+index+2);i++){
+    for(int i = max_ind; i<(max_ind+max_len);i++){
         printf("%c",string_1[i]);
     }
-    printf("\nlong:%d, index:%d",long_word,index);
+    printf("\n");
     return 0;
 }
