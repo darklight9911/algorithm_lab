72bccd1c555d43959e74fef663590ad72c843099
darklight9911 <muniaedu911@gmail.com>
2025-02-08 00:09:08 +0600

    solved 2 & 3

---
+++ lab-3/3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int findLastPosition(int arr[], int size, int key) {
+    int low = 0, high = size - 1;
+    int result = -1; 
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key) {
+            result = mid; 
+            low = mid + 1; 
+        } else if (arr[mid] < key) {
+            low = mid + 1; 
+        } else {
+            high = mid - 1; 
+        }
+    }
+    return result;
+}
+int main() {
+    int arr[] = {1, 2, 2, 2, 3, 4, 5}; 
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 2; 
+    int position = findLastPosition(arr, size, key);
+    if (position != -1) {
+        printf("The last position of %d is: %d\n", key, position);
+    } else {
+        printf("The number %d is not present in the array.\n", key);
+    }
+    return 0;
+}
